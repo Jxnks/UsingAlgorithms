e0d7ebfb4511d993a4796e16b658d22571551d85
Jxnks <68285391+Jxnks@users.noreply.github.com>
2023-01-23 14:01:59 +0000

    Selection Sort Algorithm Testing program written in main for the algorithms

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,18 +1,56 @@
 package com.company;
 
-import java.awt.print.Book;
+
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
 
+        // The program below is for testing algorithms
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<BooksToSearch> books = new ArrayList<BooksToSearch>();
+        System.out.println("How many books to create?");
+        int numberOfBooks = Integer.parseInt(scanner.nextLine());
+        for (int i = 0; i < numberOfBooks; i++) {
+            books.add(new BooksToSearch(i, "name for the book " + i));
+        }
 
+        System.out.println("Id of the book to search for?");
+        int idToSearchFor = Integer.parseInt(scanner.nextLine());
 
+        System.out.println("");
+        System.out.println("Searching with linear search:");
+        long start = System.currentTimeMillis();
+        int linearSearchId = linearSearch(books, idToSearchFor);
+        System.out.println("The search took " + (System.currentTimeMillis() - start) + " milliseconds.");
+        if (linearSearchId < 0) {
+            System.out.println("Book not found");
+        } else {
+            System.out.println("Found it! " + books.get(linearSearchId));
+        }
 
+        System.out.println("");
+
+        System.out.println("");
+        System.out.println("Seaching with binary search:");
+        start = System.currentTimeMillis();
+        int binarySearchId = binarySearch(books, idToSearchFor);
+        System.out.println("The search took " + (System.currentTimeMillis() - start) + " milliseconds.");
+        if (binarySearchId < 0) {
+            System.out.println("Book not found");
+        } else {
+            System.out.println("Found it! " + books.get(binarySearchId));
+        }
 
     }
+
+
+
+
+
+
     public static int linearSearch(ArrayList<BooksToSearch> books, int searchedId) {
         int i = 0;
         for (BooksToSearch temp : books) {
